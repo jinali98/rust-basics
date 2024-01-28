4239d921b897fd8fbf370b921665331db0f23eb7
jinali98 <jinalipabasara@gmail.com>
2024-01-28 12:29:29 +0530

    added crates, completed comparison part

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,13 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("<--- Guess the Number Game --->");
+
+    let secret_number = rand::thread_rng().gen_range(1..=10);
+
+    println!("Secret number is : {secret_number}");
 
     println!("Input your guess");
 
@@ -15,6 +21,18 @@ fn main() {
         .read_line(&mut guess)
         .expect("Cannot read the input");
 
+    // shadowing allow to re use the same variable name to conver the value of guess from 
+    // string type to u32 number type.
+    // shadowning is diff from mutating.
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
     println!("You guessed: {guess}");
+
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less =>  println!("too small"),
+        Ordering::Greater => println!("too big"),
+        Ordering::Equal => println!("correct number"),
+        
+    }
 
 }
