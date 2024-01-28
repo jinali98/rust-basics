7fb26488101a6d15574385682b4a1848aa275b72
jinali98 <jinalipabasara@gmail.com>
2024-01-28 12:44:22 +0530

    added error handling

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -9,30 +9,42 @@ fn main() {
 
     println!("Secret number is : {secret_number}");
 
-    println!("Input your guess");
+    loop {
+        println!("Input your guess");
+    
+        // mutable variable 
+        let mut guess = String::new();
+    
+    
+        // this is to get the user input and store that in the guess var
+        // & is used to refer the same var instead of creating copies of it
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Cannot read the input");
+    
+        // shadowing allow to re use the same variable name to conver the value of guess from 
+        // string type to u32 number type.
+        // shadowning is diff from mutating.
+        let guess: u32 = match guess.trim().parse(){
+        // parse returns Results which has Ok and Err variants. 
+        // match checks for the matching ARMS patterns and proceed with it
+                Ok(num) => num,
+                Err(_) => continue,
+        };
 
-    // mutable variable 
-    let mut guess = String::new();
-
-
-    // this is to get the user input and store that in the guess var
-    // & is used to refer the same var instead of creating copies of it
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Cannot read the input");
-
-    // shadowing allow to re use the same variable name to conver the value of guess from 
-    // string type to u32 number type.
-    // shadowning is diff from mutating.
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    println!("You guessed: {guess}");
-
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less =>  println!("too small"),
-        Ordering::Greater => println!("too big"),
-        Ordering::Equal => println!("correct number"),
-        
+        println!("You guessed: {guess}");
+    
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less =>  println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("YAY you are correct!!");
+                break;
+            },
+            
+        }
     }
+
 
 }
